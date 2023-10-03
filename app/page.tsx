2a56b6ae3c984b9e495c992c8e2d250489b17dcc
David Huang <david.zq.huang@gmail.com>
2023-10-02 23:42:04 -0400

    chat interaction

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,10 @@
 
 import { useState } from "react"
 
+import { Message } from 'ai/react';
+import { useChat } from 'ai/react';
+import { ChatRequest, FunctionCallHandler, nanoid } from 'ai';
+
 import Link from "next/link"
 
 import {
@@ -22,6 +26,52 @@ export default function Home() {
 
   const [sendHover, setSendHover] = useState(false)
 
+
+  // Chat Section **************************************************************
+
+  const functionCallHandler: FunctionCallHandler = async (
+    chatMessages,
+    functionCall,
+  ) => {
+    if (functionCall.name === 'eval_code_in_browser') {
+      if (functionCall.arguments) {
+        // Parsing here does not always work since it seems that some characters in generated code aren't escaped properly.
+        const parsedFunctionCallArguments: { code: string } = JSON.parse(
+          functionCall.arguments,
+        );
+        // WARNING: Do NOT do this in real-world applications!
+        eval(parsedFunctionCallArguments.code);
+        const functionResponse = {
+          messages: [
+            ...chatMessages,
+            {
+              id: nanoid(),
+              name: 'eval_code_in_browser',
+              role: 'function' as const,
+              content: parsedFunctionCallArguments.code,
+            },
+          ],
+        };
+        return functionResponse;
+      }
+    }
+  };
+
+  const { messages, input, handleInputChange, handleSubmit, data } = useChat({
+    api: '/api/chat',
+    experimental_onFunctionCall: functionCallHandler,
+  });
+
+  // Generate a map of message role to text color
+  const roleToColorMap: Record<Message['role'], string> = {
+    system: 'red',
+    user: 'black',
+    function: 'blue',
+    assistant: 'green',
+  };
+
+  // End Chat Section **********************************************************
+
   return (
     <div className="flex flex-col h-[calc(100vh-100px)] space-y-4">
       <div className="flex-grow mt-4" style={{ flex: '1' }}>
@@ -41,21 +91,53 @@ export default function Home() {
 
       </div>
       <div className="flex-grow" style={{ flex: '3' }}>
-        {/* Content for the second (3/5 height) div */}
-        <p>Chat</p>
+        <div className="flex flex-col w-full mx-auto stretch">
+          {messages.length > 0
+            ? messages.map((m: Message) => (
+              <div
+                key={m.id}
+                className="whitespace-pre-wrap"
+                style={{ color: roleToColorMap[m.role] }}
+              >
+                <strong>{`${m.role}: `}</strong>
+                {m.content || JSON.stringify(m.function_call)}
+                <br />
+                <br />
+              </div>
+            ))
+            : null}
+        </div>
       </div>
-      <div className="flex-grow flex flex-row space-x-4 items-center" style={{ flex: '1' }}>
-        <div className="w-4/5">
-          <Textarea placeholder="Type your message here." />
-        </div>
-        <div className="w-1/5" onMouseEnter={() => setSendHover(true)} onMouseLeave={() => setSendHover(false)}>
-          {
-            sendHover ?
-              <BiPaperPlane className="h-10 w-10 text-blue-500 hover:text-blue-600 cursor-pointer" />
-              :
-              <BiPaperPlane className="h-10 w-10 text-gray-500 hover:text-gray-600 cursor-pointer" />
-          }
-        </div>
+      <div className="flex-grow" style={{ flex: '1' }}>
+        <form onSubmit={(e) => {
+          console.log("SUBMIT")
+          handleSubmit(e)
+          console.log("input submit: " + e)
+        }
+        }>
+          <div className="flex flex-row space-x-4 items-center">
+            <div className="w-4/5">
+              <Textarea placeholder="Type your message here." value={input} onChange={(e) => {
+                handleInputChange(e)
+                console.log("input: " + e.target.value)
+              }} />
+            </div>
+            <div
+              className="w-1/5 cursor-pointer"
+              onMouseEnter={() => setSendHover(true)}
+              onMouseLeave={() => setSendHover(false)}
+            >
+              <button
+                type="submit"
+                className="cursor-pointer">
+                {sendHover ?
+                  <BiPaperPlane className="h-10 w-10 text-black" /> :
+                  <BiPaperPlane className="h-10 w-10 text-gray-500 hover:text-gray-600" />
+                }
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
     </div>
   );
--- components/mdx-components.tsx
+++ components/mdx-components.tsx
@@ -1,16 +1,27 @@
-import Image from "next/image"
-import { useMDXComponent } from "next-contentlayer/hooks"
+import Image from "next/image";
+import { useMDXComponent } from "next-contentlayer/hooks";
 
 const components = {
   Image,
-}
+  h1: (props: any) => <h1 className="mt-6 mb-4 text-4xl font-bold" {...props} />,
+  h2: (props: any) => <h2 className="mt-2 mb-2 text-3xl font-bold" {...props} />,
+  h3: (props: any) => <h3 className="text-2xl font-bold" {...props} />,
+  h4: (props: any) => <h4 className="text-xl font-bold" {...props} />,
+  h5: (props: any) => <h5 className="text-lg font-bold" {...props} />,
+  h6: (props: any) => <h6 className="text-md font-bold" {...props} />,
+  a: (props: any) => <a className="text-blue-500 hover:underline" {...props} />,
+  p: (props: any) => <p className="text-md mt-2" {...props} />,
+  ol: (props: any) => <ol className="list-decimal list-inside" {...props} />,
+  ul: (props: any) => <ul className="list-disc list-inside" {...props} />,
+  li: (props: any) => <li className="text-md mt-2" {...props} />,
+};
 
 interface MdxProps {
-  code: string
+  code: string;
 }
 
 export function Mdx({ code }: MdxProps) {
-  const Component = useMDXComponent(code)
+  const Component = useMDXComponent(code);
 
-  return <Component components={components} />
+  return <Component components={components} />;
 }

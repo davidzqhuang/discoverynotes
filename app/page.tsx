6dfd2d249c2e0f380c220e49648d432146d8d0e9
David Huang <david.zq.huang@gmail.com>
2023-10-02 22:31:31 -0400

    layout

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,19 @@ import Link from "next/link"
 
 export default function Home() {
   return (
-    <div className="prose dark:prose-invert">
-      
+    <div className="flex flex-col h-screen">
+      <div className="flex-grow" style={{ flex: '1' }}>
+        {/* Content for the first (1/5 height) div */}
+        <p>Notes</p>
+      </div>
+      <div className="flex-grow" style={{ flex: '3' }}>
+        {/* Content for the second (3/5 height) div */}
+        <p>Chat</p>
+      </div>
+      <div className="flex-grow" style={{ flex: '1' }}>
+        {/* Content for the third (1/5 height) div */}
+        <p>Text</p>
+      </div>
     </div>
-  )
+  );
 }
